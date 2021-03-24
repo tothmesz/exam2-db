c9735fbf2e560d7c18b15536ccdb0ce08ff711d9
tothmesz <66495712+tothmesz@users.noreply.github.com>
2021-03-24 17:32:37 +0100

    finished2

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,38 @@
 package com.codecool.database;
 
+import java.sql.*;
 
 public class RadioCharts {
-    //TODO: implement this class
+    String url;
+    String user;
+    String password;
+
+    public RadioCharts(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getMostPlayedSong() {
+        String query = "SELECT song FROM music_broadcast GROUP BY song ORDER BY SUM(times_aired) DESC LIMIT 2";
+        return getResult(query, "song");
+    }
+
+    public String getMostActiveArtist() {
+        String query = "SELECT COUNT(DISTINCT song) AS number_of_songs, artist FROM music_broadcast GROUP BY artist ORDER BY number_of_songs DESC LIMIT 1";
+        return getResult(query, "artist");
+    }
+
+    private String getResult(String query, String column) {
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            if (rs.next()) {
+                return (rs.getString(column));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
